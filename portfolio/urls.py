b84e368b6f30cb84d45974a9a6d9e36f3506e769
tsaqifmaulana444 <tsaqifdepok@gmail.com>
2023-06-29 19:41:29 +0700

    config

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -13,16 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
-from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+   
 ]
